f8c2ae86c6c0d19c5e6af2abc05c101fa6221940
IamAshish2 <karkiaashish9899@gmail.com>
2025-01-25 09:44:43 +0545

    fix code block error (swagger)

--- MinimalAPIConcepts/Program.cs
+++ MinimalAPIConcepts/Program.cs
@@ -3,16 +3,18 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+// Add the Swagger Service to the application
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minimal Api Practice", Description = "Say hello minimal api", Version = "v1" });
+});
+
 
 var app = builder.Build();
 
+// actually use the swagger ui
 if (app.Environment.IsDevelopment())
 {
-    builder.Services.AddSwaggerGen((c) =>
-    {
-    c.SwaggerDoc("v1",new OpenApiInfo { Title = "Minimal Api Practice", Description = "Say hello minimal api", Version = "v1"});
-    });
-
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
